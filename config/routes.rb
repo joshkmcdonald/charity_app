ea0e2b2d1f7283efba90ed08430fa36df4819750
joshkmcdonald <joshkmcdonald@gmail.com>
2017-03-15 14:33:05 -0600

    Generate pages controller with home, about, and contact pages Set home as root page in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'pages/about'
+  get 'about', to: 'pages#about'
+  # get 'pages/contact'
+  get 'contact', to: 'pages#contact'
+  
+  root :to => "pages#home"
 end

506bef4e9b74bfba7497cc26826f5074a4ac694c
joshkmcdonald <joshkmcdonald@gmail.com>
2017-03-16 17:08:52 -0600

    Move authentication before_action from pages controller to donations_controller

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,5 +1,6 @@
 class DonationsController < ApplicationController
-  before_action only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action only: [:index, :show, :edit, :update, :destroy]
 
 
   def index
